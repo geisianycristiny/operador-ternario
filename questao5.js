9199a25db29cdcc3067a0e1dfc33002a43b36d1a
Ana Eloize <anaeloizea@gmail.com>
2024-04-24 11:08:17 -0400

    questao finalizada

---
+++ questao5.js
@@ -0,0 +1,9 @@
+/*
+Questão 5: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se uma pessoa é maior de idade ou menor de idade. A variável 
+idade contém a idade da pessoa. Atribua o resultado à variável status. 
+ */
+resposta
+let idade = 20;
+let estado = (idade >=18)? "adulto" : "menor de idade";
+console.log(estado);
