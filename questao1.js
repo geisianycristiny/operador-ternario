030d9646434ebe9479ed175648286d76a0daf824
Ana Eloize <anaeloizea@gmail.com>
2024-04-24 11:06:57 -0400

    questao finalizada

---
+++ questao1.js
@@ -0,0 +1,8 @@
+/* Questão 1: Escreva um código JavaScript utilizando o operador ternário para 
+*/
+
+const numero = 10; // Pode ser qualquer número que você queira verificar
+
+const resultado = numero % 2 === 0 ? 'par' : 'ímpar';
+
+console.log(`O número ${numero} é ${resultado}.`);
